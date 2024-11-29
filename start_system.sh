151b7a568929e4fd57c18ff372e4e3fe936ee9f2
egadams <eg1972@gmail.com>
2024-11-29 12:44:08 -0500

    save the start_system script

---
+++ start_system.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# 
+# first friday
+#
+# script to start the agent communication system
+
+
+# define the aliases used in this script
+source ~/.bashrc
+
+# go into the correct environment
+so;
+
+# set the OPENAI_API_KEY
+oai;
+
+# run the Python start system script
+python3 start_system.py;
+
+# the end; hello first friday
