3e470d183dc8f892f59ea4cd4c00589f616dd167
egadams <eg1972@gmail.com>
2024-12-15 10:30:48 -0500

    changed openai script to open door

--- start_system.sh
+++ start_system.sh
@@ -5,7 +5,7 @@
 # script to start the agent communication system
 
 
-source ~/the_function_caller/set_openai_key.sh
+source ~/the_function_caller/open_door.sh
 
 # go into the correct environment
 # so;
